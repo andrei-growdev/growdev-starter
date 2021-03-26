74ec80e24369948ab5008d997d2e7a588ff13a66
andrei.brando <andrei.brando@growdev.com.br>
2021-03-26 08:50:58 -0300

    commit

--- semana05/ex_repeticao/js/scripts.js
+++ semana05/ex_repeticao/js/scripts.js
@@ -14,19 +14,23 @@ for (let i = 1; i <= 10; i++) {
 }
 
 //#4
-const meses = ['Janeiro', 'Fevereiro','Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto','Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 // console.log(meses[0]);
 // console.log(meses[meses.length-1]);
 
 //#5
-for (let i = meses.length-1; i >= 0; i--) {
+for (let i = meses.length - 1; i >= 0; i--) {
    // console.log(meses[i]);
+}
+
+for (mes of meses.reverse()) {
+   // console.log(mes);
 }
 
 //#6
 const notas = [];
 for (let i = 1; i <= 10; i++) {
-   notas.push((i * 8) +3);
+   notas.push((i * 8) + 3);
 }
 // console.log(notas);
 
@@ -45,4 +49,76 @@ while (iterator <= 10) {
    arrayInteiros.push(iterator);
    iterator++;
 }
-console.log(arrayInteiros);
+// console.log(arrayInteiros);
+
+const tenis = {
+   cor: 'preto',
+   marca: 'nike',
+   tamanho: 42,
+   teste: [],
+   adicionar: function (valor) {
+      this.teste.push(valor);
+   },
+   exibirCor() {
+      console.log(`A cor é ${this.cor}`);
+   }
+}
+// tenis.adicionar('outro teste');
+// tenis.exibirCor();
+
+const aluno = {
+   nome: 'Andrei',
+   sobrenome: 'Brando',
+   idade: 18,
+   notas: [],
+   exibirNomeCompleto() {
+      console.log(`${this.nome} - ${this.sobrenome}`);
+   },
+   adicionarNotas(nota) {
+      this.notas.push(nota);
+   },
+   exibirMedia() {
+      let media = 0;
+      for (nota of this.notas) {
+         media += nota;
+      }
+      media = media / this.notas.length;
+      console.log(media);
+      if (media > 7) {
+         console.log('Passou de ano');
+      } else {
+         console.log('Reprovou');
+      }
+   },
+   exibirTurma() {
+      switch (this.idade) {
+         case 15:
+            console.log('1º ano');
+            break;
+         case 16:
+            console.log('2º ano');
+            break;
+         case 17:
+            console.log('3º ano');
+            break;
+         case 18:
+            console.log('4º ano');
+            break;
+         default:
+            break;
+      }
+   },
+   exibirNotas() {
+      for (nota of this.notas) {
+         console.log(`Nota => ${nota}`);
+      }
+   }
+
+}
+aluno.exibirNomeCompleto();
+aluno.adicionarNotas(10);
+aluno.adicionarNotas(5);
+aluno.adicionarNotas(10);
+aluno.exibirMedia();
+aluno.exibirTurma();
+aluno.exibirNotas();
